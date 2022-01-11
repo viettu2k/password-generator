d15242dd89164074e7f848dd068857cd5934f77c
viettu2k <doanviettu2k@gmail.com>
2022-01-11 09:40:24 +0700

    generating a random password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,7 +30,26 @@ export class AppComponent {
   }
 
   onButtonClick() {
-    console.log(this.length);
-    this.password = 'dark dark bruh bruh lmao';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
   }
 }
